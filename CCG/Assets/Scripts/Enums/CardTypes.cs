1d54ff1df9470340e8c786a0b047740d67253eab
Ricky <ricky.wiggins91@gmail.com>
2016-12-22 00:20:20 -0600

    project creation
    created unity project

---
+++ CCG/Assets/Scripts/Enums/CardTypes.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardTypes : MonoBehaviour {
+
+	enum CardType {minion, spell};
+
+	CardType type;
+	public int cost;
+
+	//minion card variables
+	public int health;
+	public int attack;
+
+	//spell card variables
+	public string description;
+	public string function;
+
+	// Use this for initialization
+	void Start () {
+		//retrieve card info from DB
+		string sql = "";
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
