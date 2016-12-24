adaf2663dd52179fcdcad82310d6280022913a0a
Ricky <ricky.wiggins91@gmail.com>
2016-12-24 11:13:28 -0600

    Card code corrected
    Moved card code from cardType to card. cardType now has only the card
    type enum as its supposed to

--- CCG/Assets/Scripts/Card.cs
+++ CCG/Assets/Scripts/Card.cs
@@ -3,15 +3,34 @@ using System.Collections;
 
 public class Card : MonoBehaviour {
 	
-
-
+	
+	enum CardType {minion, spell};
+	
+	CardType type;
+	public int cost;
+	public string name;
+	
+	//minion card variables
+	public int health;
+	public int attack;
+	
+	//spell card variables
+	public string description;
+	public string function;
+	
 	// Use this for initialization
 	void Start () {
-	
+		//retrieve card info from DB
+		string sql = "";
+		sql = "SELECT * FROM Cards WHERE Title = name";
+		
+		//here we would put the sql data readers and other things like that
+		//still trying to figure out how to setup DB with unity
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
